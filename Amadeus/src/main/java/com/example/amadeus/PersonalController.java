091db4c6d5ba02284a039ca128ea84edde5c2585
AudwitNafi <85760953+AudwitNafi@users.noreply.github.com>
2022-03-24 19:13:05 +0600

    Delete Amadeus directory

--- Amadeus/src/main/java/com/example/amadeus/PersonalController.java
+++
@@ -1,4 +0,0 @@
-package com.example.amadeus;
-
-public class PersonalController {
-}
--- Amadeus/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.amadeus {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.amadeus to javafx.fxml;
-    exports com.example.amadeus;
-}
